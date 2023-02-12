12d5befdeedc3e47f208ab5e55568c62d3969ad1
whosmudassir <deadsoulartist@gmail.com>
2023-02-12 09:25:31 +0530

    feat: scrollview on fav items

--- knowhere/src/screens/FavoriteFoodItems/index.tsx
+++ knowhere/src/screens/FavoriteFoodItems/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Text, VStack, View} from 'native-base';
+import {Text, VStack, View, ScrollView} from 'native-base';
 import {foodItems} from '../../data/FoodItemsMockData';
 import FoodAdItem from '../../components/common/FoodAdItem';
 import {userFoodFavoriteStore} from '../../store';
@@ -14,7 +14,7 @@ const FavoriteFoodItems = () => {
   });
 
   return (
-    <>
+    <ScrollView flex={1} bgColor={'secondary'}>
       <VStack alignItems={'center'}>
         <Text variant={'categoryTitle'}></Text>
         <VStack w={'90%'}>
@@ -25,7 +25,7 @@ const FavoriteFoodItems = () => {
           ))}
         </VStack>
       </VStack>
-    </>
+    </ScrollView>
   );
 };
 
