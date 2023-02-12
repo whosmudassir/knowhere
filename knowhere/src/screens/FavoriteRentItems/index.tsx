d1df3fc665d3da9f1447a6d2db1c3b15920fe706
whosmudassir <deadsoulartist@gmail.com>
2023-02-12 08:26:18 +0530

    feat: fav item tab ui

--- knowhere/src/screens/FavoriteRentItems/index.tsx
+++ knowhere/src/screens/FavoriteRentItems/index.tsx
@@ -1,8 +1,32 @@
-import {Text} from 'native-base';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Text, VStack, View} from 'native-base';
+import {rentItems} from '../../data/RentItemsMockData';
+import RentAdItem from '../../components/common/RentAdItem';
+import {userRentFavoriteStore} from '../../store';
 
 const FavoriteRentItems = () => {
-  return <Text>FavoriteRent</Text>;
+  const userRentFavorites = userRentFavoriteStore(
+    state => state.userRentFavorites,
+  );
+
+  const filteredRentItems = rentItems.filter(item => {
+    return userRentFavorites.includes(item.id);
+  });
+
+  return (
+    <>
+      <VStack alignItems={'center'}>
+        <Text variant={'categoryTitle'}>Places for rent</Text>
+        <VStack w={'90%'}>
+          {filteredRentItems.map(item => (
+            <View key={item.id}>
+              <RentAdItem item={item} />
+            </View>
+          ))}
+        </VStack>
+      </VStack>
+    </>
+  );
 };
 
 export default FavoriteRentItems;
